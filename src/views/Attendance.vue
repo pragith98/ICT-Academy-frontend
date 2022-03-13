db23950c5f393d10a5d930c854e563a2e53b2a65
pragith98 <lakshanxp@hotmail.com>
2022-03-13 18:09:26 +0530

    create attendance card

--- src/views/Attendance.vue
+++ src/views/Attendance.vue
@@ -1,3 +1,56 @@
 <template>
-    <div>Attendance page</div>
+    <div>
+        <v-breadcrumbs :items="breadcrumbs">
+            <template v-slot:divider>
+                <v-icon>mdi-chevron-right</v-icon>
+            </template>
+        </v-breadcrumbs>
+
+
+        <v-container>
+            <h1 class="title font-weight-black grey--text">ATTENDANCE</h1>
+            <v-row>
+                <v-col lg="3" md="4" sm="6" cols="12" v-for="cardlist in cardlist" :key="cardlist.name">
+                    <v-hover close-delay="100" v-slot="{hover}">
+                        <v-card  :elevation="hover? 7:0" flat class="'on-hover': hover lighten-2" :to=cardlist.link :color=cardlist.color dark>
+                        
+                            <v-responsive class="pt-4 text-center">
+                                <v-icon size="100">{{cardlist.icon}}</v-icon>
+                            </v-responsive>
+                            <v-card-title class=" mb-5" style="justify-content:center">
+                                <div class="title" >{{ cardlist.name }}</div>
+                            </v-card-title>
+                            
+                        </v-card>
+                    </v-hover>
+                    
+                </v-col>
+
+            </v-row>
+
+        </v-container>
+    </div>
 </template>
+
+
+
+<script>
+
+    export default{
+
+        data(){
+            return{
+                cardlist:[
+                    {name:'Mark Attendance', icon:'mdi-checkbox-marked-outline', color:'red', link:'/Attendance'},
+                    {name:'Attendance Details', icon:'mdi-view-list', color:'orange', link:'/Attendance'}
+                ],
+
+
+                breadcrumbs: [
+                    { text: 'Attendance', disabled: true, href: '/Attendance' }
+                ],
+            }
+        }
+        
+    }
+</script>
