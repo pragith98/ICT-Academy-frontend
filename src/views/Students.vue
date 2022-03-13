36678001907081d0029edb7823ba2919f51fd01a
pragith98 <lakshanxp@hotmail.com>
2022-03-13 17:21:26 +0530

    create student card

--- src/views/Students.vue
+++ src/views/Students.vue
@@ -1,6 +1,56 @@
 <template>
-    <div>Student page
+    <div>
+        <v-breadcrumbs :items="breadcrumbs">
+            <template v-slot:divider>
+                <v-icon>mdi-chevron-right</v-icon>
+            </template>
+        </v-breadcrumbs>
 
-        <router-link to="/Students/StudentRegistration">Student Registration</router-link>
+
+        <v-container>
+            <h1 class="title font-weight-black grey--text">STUDENTS</h1>
+            <v-row>
+                <v-col lg="3" md="4" sm="6" cols="12" v-for="cardlist in cardlist" :key="cardlist.name">
+                    <v-hover close-delay="100" v-slot="{hover}">
+                        <v-card  :elevation="hover? 7:0" flat class="'on-hover': hover lighten-2" :to=cardlist.link :color=cardlist.color dark>
+                        
+                            <v-responsive class="pt-4 text-center">
+                                <v-icon size="100">{{cardlist.icon}}</v-icon>
+                            </v-responsive>
+                            <v-card-title class=" mb-5" style="justify-content:center">
+                                <div class="title" >{{ cardlist.name }}</div>
+                            </v-card-title>
+                            
+                        </v-card>
+                    </v-hover>
+                    
+                </v-col>
+
+            </v-row>
+
+        </v-container>
     </div>
 </template>
+
+
+
+<script>
+
+    export default{
+
+        data(){
+            return{
+                cardlist:[
+                    {name:'Register Students', icon:'mdi-account-plus', color:'teal', link:'/Students/StudentRegistration'},
+                    {name:'Student Details', icon:'mdi-account-plus', color:'orange', link:'/Students'}
+                ],
+
+
+                breadcrumbs: [
+                    { text: 'Students', disabled: true, href: '/Students' }
+                ],
+            }
+        }
+        
+    }
+</script>
