ca4c7b185ba0f9625c465cf6307908fd6e1ba047
pragith98 <lakshanxp@hotmail.com>
2022-03-13 18:29:44 +0530

    create payments cards

--- src/views/Payments.vue
+++ src/views/Payments.vue
@@ -1,3 +1,56 @@
 <template>
-    <div>payments page</div>
+    <div>
+        <v-breadcrumbs :items="breadcrumbs">
+            <template v-slot:divider>
+                <v-icon>mdi-chevron-right</v-icon>
+            </template>
+        </v-breadcrumbs>
+
+
+        <v-container>
+            <h1 class="title font-weight-black grey--text">PAYMENTS</h1>
+            <v-row>
+                <v-col lg="3" md="4" sm="6" cols="12" v-for="cardlist in cardlist" :key="cardlist.name">
+                    <v-hover close-delay="100" v-slot="{hover}">
+                        <v-card  :elevation="hover? 7:0" flat class="'on-hover': hover lighten-2" :to=cardlist.link :color=cardlist.color dark>
+                        
+                            <v-responsive class="pt-4 text-center">
+                                <v-icon size="100">{{cardlist.icon}}</v-icon>
+                            </v-responsive>
+                            <v-card-title class=" mb-5" style="justify-content:center">
+                                <div class="title" >{{ cardlist.name }}</div>
+                            </v-card-title>
+                            
+                        </v-card>
+                    </v-hover>
+                    
+                </v-col>
+
+            </v-row>
+
+        </v-container>
+    </div>
 </template>
+
+
+
+<script>
+
+    export default{
+
+        data(){
+            return{
+                cardlist:[
+                    {name:'Mark Class fees', icon:'mdi-currency-usd', color:'pink lighten-2', link:'/Payments'},
+                    {name:'Payment Details', icon:'mdi-view-list', color:'orange', link:'/Payments'}
+                ],
+
+
+                breadcrumbs: [
+                    { text: 'Payments', disabled: true, href: '/Payments' }
+                ],
+            }
+        }
+        
+    }
+</script>
