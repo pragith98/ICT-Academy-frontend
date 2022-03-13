eefce7e18e97ceae79bfee8350a1036f08a5a11f
pragith98 <lakshanxp@hotmail.com>
2022-03-13 14:38:28 +0530

    add cards to staff page

--- src/views/Staff.vue
+++ src/views/Staff.vue
@@ -5,10 +5,30 @@
                 <v-icon>mdi-chevron-right</v-icon>
             </template>
         </v-breadcrumbs>
+
+
         <v-container>
+            <h1 class="title font-weight-black grey--text">STAFF</h1>
+            <v-row>
+                <v-col lg="3" md="4" sm="6" cols="12" v-for="cardlist in cardlist" :key="cardlist.name">
+                    <v-hover close-delay="100" v-slot="{hover}">
+                        <v-card  :elevation="hover? 7:0" flat class="'on-hover': hover" :to=cardlist.link :color=cardlist.color dark>
+                        
+                            <v-responsive class="pt-4 text-center">
+                                <v-icon size="100">{{cardlist.icon}}</v-icon>
+                            </v-responsive>
+                            <v-card-title class=" mb-5" style="justify-content:center">
+                                <div class="title" >{{ cardlist.name }}</div>
+                            </v-card-title>
+                            
+                        </v-card>
+                    </v-hover>
+                    
+                </v-col>
+
+            </v-row>
 
         </v-container>
-        <router-link to="/Staff/StaffRegistration">Register Staff</router-link>
     </div>
 </template>
 
@@ -20,6 +40,12 @@
 
         data(){
             return{
+                cardlist:[
+                    {name:'Register Staff', icon:'mdi-account-plus', color:'teal', link:'/Staff/StaffRegistration'},
+                    {name:'Staff Details', icon:'mdi-account-plus', color:'orange', link:'/Staff'}
+                ],
+
+
                 breadcrumbs: [
                     { text: 'Staff', disabled: true, href: '/Staff' }
                 ],
