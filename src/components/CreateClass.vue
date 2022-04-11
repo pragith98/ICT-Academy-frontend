320fa11c7dfed91ff53fc037e82f32167fceac9d
pragith98 <lakshanxp@hotmail.com>
2022-04-12 05:10:22 +0530

    complete create class

--- src/components/CreateClass.vue
+++ src/components/CreateClass.vue
@@ -8,10 +8,21 @@
             </v-breadcrumbs>
         <v-container>
 
+            <!----------------------------- Alerts ---------------------------------->
+
             <v-snackbar :timeout="3000" v-model="unsuccessAlert" color="red"  bottom ><v-icon left>mdi-alert-outline</v-icon> Staff delete <strong>failed</strong> </v-snackbar>
             <v-snackbar :timeout="3000" v-model="successAlert" color="green"  bottom><v-icon left>mdi-check</v-icon>Staff delete <strong>successful</strong> </v-snackbar>
             
+            <v-snackbar v-model="successfulCreateSubject" :multi-line="multiLine">
+                Subject Create successfully. Now you can add it from the table.
+                <template v-slot:action="{ attrs }">
+                    <v-btn color="red" text v-bind="attrs" @click="successfulCreateSubject = false">Close</v-btn>
+                </template>
+            </v-snackbar>
+
+            <v-snackbar :timeout="3000" v-model="unsuccessfulCreateSubject" color="red"  bottom ><v-icon left>mdi-alert-outline</v-icon> Subject Create <strong>failed</strong> </v-snackbar>
             
+            <!----------------------------- Alerts ---------------------------------->
  
 
             <template>
@@ -93,8 +104,7 @@
                                     <v-card-actions>
                                         <v-spacer></v-spacer>
                                         <v-btn outlined color="grey" @click="Reset(),scrollToTop()">Cancel</v-btn>
-                                        <!-- <v-btn color="primary" @click="e1=2" depressed :disabled="!valid || !className || !getFeeType || !fee || !getLocation || !getDay || !startTime || !endTime">Next -->
-                                        <v-btn color="primary" @click="e1=2" depressed >Next
+                                        <v-btn color="primary" @click="e1=2" depressed :disabled="!valid || !className || !getFeeType || !fee || !getLocation || !getDay || !startTime || !endTime">Next
                                             <v-icon>mdi-chevron-right</v-icon>
                                         </v-btn>
                                     </v-card-actions>
@@ -132,55 +142,99 @@
                                 <v-stepper-content step="3">
                                     <v-card class="mb-12" flat min-height="200px">
                                         <v-row dense>
-                                            <v-col cols="12" md="6" sm="6">
-                                                <v-card-text>
-                                                    <v-autocomplete :items="subjects" v-model="subject" :filter="subjectFilter" item-text="name" item-value="subjectID" label="Subject" prepend-icon="mdi-format-align-center" :rules="teacherRules"></v-autocomplete>
-                                                </v-card-text>
+                                            <v-col cols="12" md="12" sm="12">
+                                                <v-text-field v-model="subjectName" label="Subject" prepend-icon="mdi-format-align-center" :rules="subjectRules" required  readonly hint="Add subject from below table"></v-text-field>
                                             </v-col>
-                                            <v-col cols="12" md="6" sm="6" align-self="center">
-                                                <v-card-text class="grey--text">
-                                                    * Use Name, ID, Category or Medium to search for a subject 
-                                                </v-card-text>
+                                            <v-card-text class="grey--text">
+                                                * Add subject from below table
+                                            </v-card-text>
+                                            
+                                           
+                                            <v-col cols="12" md="12" sm="12">
+                                                <v-card-title><v-spacer></v-spacer><v-text-field v-model="subjectSearch" append-icon="mdi-magnify" label="Search Subject" single-line hide-details></v-text-field></v-card-title>
+                                                <v-data-table :headers="subjectHeaders" :items="subjects" :search="subjectSearch" :items-per-page="5">
+                                                    <template v-slot:item="row">
+                                                        <tr>
+                                                            <td>{{row.item.name}}</td>
+                                                            <td>{{row.item.medium}}</td>
+                                                            <td>{{row.item.category}}</td>
+                                                            <td>
+                                                                <v-btn color="primary" outlined @click="subjectName=row.item.name, subject=row.item.id">Add</v-btn>
+                                                            </td>
+                                                        </tr>
+                                                    </template>
+                                                </v-data-table>
                                             </v-col>
-
+                                            
                                         </v-row>
+                                        <v-divider></v-divider>
+                                        <v-card-text>
+                                            * If you won't find the Subject from the table,
+                                        </v-card-text>
+                                        <div class="mt-5">
+                                            <app-createSubject @success="successfulCreateSubjectAlert($event)" @failed="unsuccessfulCreateSubjectAlert($event)"></app-createSubject>
+                                        </div>
                                         
                                     </v-card>
+                                    <v-divider></v-divider>
                                     <v-card-actions>
                                         <v-spacer></v-spacer>
                                         <v-btn outlined color="grey" @click="e1=2">
                                             <v-icon >mdi-chevron-left</v-icon>
                                             Back
                                         </v-btn>
-                                        <v-btn color="primary" @click="e1=4" depressed>Next
+                                        <v-btn color="primary" @click="e1=4" depressed :disabled="!valid || !subjectName">Next
                                             <v-icon>mdi-chevron-right</v-icon>
                                         </v-btn>
                                     </v-card-actions>
                                 </v-stepper-content>
 
                                 <v-stepper-content step="4">
-                                    <v-card color="grey" class="mb-12" height="200px" flat></v-card>
+                                    <v-card  class="mb-12 text-center" height="100px" flat>
+                                        <v-btn x-large color="primary" depressed @click="submit()">Create Class
+                                            <v-icon right>mdi-home-plus</v-icon>
+                                        </v-btn>
+                                        <br>
+                                        <v-btn text color="grey" class="mt-3" @click="Reset(), e1=1">Cancel</v-btn>
+                                    </v-card>
+
                                     <v-card-actions>
                                         <v-spacer></v-spacer>
                                         <v-btn outlined color="grey" @click="e1=3">
                                             <v-icon >mdi-chevron-left</v-icon>
                                             Back
                                         </v-btn>
-                                        <v-btn color="primary" depressed>Create Class</v-btn>
                                     </v-card-actions>
                                 </v-stepper-content>
                             </v-form>
                             
                         </v-stepper-items>
-                        
-
-                       
                     </v-stepper>
-
                 </v-card>
             </template>
 
-            
+
+            <!----------------------------------- dialog box ------------------------------------------>
+
+            <v-dialog v-model="dialog" persistent max-width="400px">
+                <v-card>
+                    <v-container class="text-center">
+                        <v-card-title>
+                            <v-row justify="center">
+                                <v-icon size="100" color="success">mdi-check-circle-outline</v-icon>
+                            </v-row>
+                        </v-card-title>
+                        <span class="text-h6 text-center">Class created <strong>successfully</strong> </span>
+                    </v-container>
+                    
+                    
+                    <v-card-actions style="justify-content: center">
+                        <v-btn color="success" depressed @click="dialog = false, Reset(), e1=1" block >Ok</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+
+            <!----------------------------------- dialog box ------------------------------------------>
             
         </v-container>
     </div>
@@ -189,14 +243,16 @@
 </template>
 
 <script>
+    import CreateSubject from './CreateSubject.vue'
     
 
     export default {
         components:{
-            
+            'app-createSubject':CreateSubject
         },
         data () {
             return {
+                dialog:false,
 
                 valid:true,
                 e1: 1,
@@ -218,6 +274,22 @@
 
                 teacher:null,
                 subject:null,
+                subjectName:null,
+
+                subjectSearch: '',
+                subjectHeaders: [
+                    {text: 'NAME',align: 'start', sortable: true, value:'name'},
+                    { text: 'MEDIUM',sortable: true, value: 'medium', filterable:false},
+                    { text: 'CATEGORY', sortable: true, value: 'category' },
+                    { text: '', sortable: false, value: 'Action' },
+                ],
+                subjects: [
+                    {name:'Science',id:'sub001',medium:'Sinhala',category:'Ordinary Level'},
+                    {name:'Maths',id:'sub002',medium:'Sinhala',category:'Ordinary Level'},
+                    {name:'Scholarship',id:'sub003',medium:'Sinhala',category:'Scholarship'},
+                    {name:'BS',id:'sub004',medium:'Sinhala',category:'Advanced Level'},
+                    {name:'Maths',id:'sub005',medium:'English',category:'Ordinary Level'}
+                ],
 
                 teachers:[
                     {name:'Samansfsdf Herathsfddf 2121', teacherID:'2121'},
@@ -226,12 +298,7 @@
                     {name:'Maheshi Ranathunga', teacherID:'3167'},
                 ],
 
-                subjects:[
-                    {name:'Sinhala, Category: OL, Medium: Sinhala', subjectID:'2121', category:'OL', medium:'Sinhala'},
-                    {name:'Science', subjectID:'2133', category:'OL', medium:'Sinhala'},
-                    {name:'Maths', subjectID:'24121', category:'OL', medium:'English'},
-                    {name:'Bs', subjectID:'3167', category:'AL', medium:'Sinhala'},
-                ],
+                
 
                 breadcrumbs: [
                     { text: 'Classes', disabled: false, href: '/Classes' },
@@ -254,6 +321,8 @@
 
                 teacherRules: [v=> !!v || 'Teacher is required'],
 
+                subjectRules: [v=> !!v || 'Subject is required'],
+
 
 
                 // -----------dropdown list-----------
@@ -265,18 +334,18 @@
 
                 successAlert:false,
                 unsuccessAlert:false,
+
+                multiLine: true,
+
+                successfulCreateSubject:false,
+                unsuccessfulCreateSubject:false,
             }
         },
 
         methods: {
-            deleteAlert(success){
-                this.successAlert = success;
-            },
-            faileAlert(failed){
-                this.unsuccessAlert = failed;
-            },
+            
 
-            Reset () {
+            Reset() {
                 this.$refs.form.reset()
             },
 
@@ -293,18 +362,28 @@
 
             },
 
-            subjectFilter (item, queryText) {
-                const textOne = item.name.toLowerCase()
-                const textTwo = item.teacherID.toLowerCase()
-                const searchText = queryText.toLowerCase()
-
-                return textOne.indexOf(searchText) > -1 || textTwo.indexOf(searchText) > -1
-
-            },
-
             submit(){
                 console.log(this.teacher);
-            }
+                this.dialog= !this.dialog
+            },
+
+
+
+            // ----------------alerts--------------------------
+            deleteAlert(success){
+                this.successAlert = success;
+            },
+            faileAlert(failed){
+                this.unsuccessAlert = failed;
+            },
+            successfulCreateSubjectAlert(success){
+                this.successfulCreateSubject = success;
+            },
+            unsuccessfulCreateSubjectAlert(failed){
+                this.unsuccessfulCreateSubject = failed;
+            },
+
+
         }
     }
 </script>
@@ -312,7 +391,11 @@
 
 
 
-
+<style scoped>
+    .v-application .justify-end {
+        justify-content: center !important;
+    }
+</style>
 
             
         
