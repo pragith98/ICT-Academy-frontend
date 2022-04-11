886248c711e1406c2a29d9d72ab3c6bf56379b3a
pragith98 <lakshanxp@hotmail.com>
2022-04-11 20:07:11 +0530

    complete create subject component

--- src/components/CreateSubject.vue
+++ src/components/CreateSubject.vue
@@ -2,108 +2,80 @@
   <v-row justify="end">
     <v-dialog v-model="dialog" scrollable max-width="700px" persistent>
         <template v-slot:activator="{ on, attrs }">
-            <v-btn class="primary" dark  depressed  v-bind="attrs" v-on="on">view<v-icon dark right>mdi-account</v-icon></v-btn>
+            <v-btn class="primary" dark  depressed  v-bind="attrs" v-on="on">Add New Subject<v-icon dark right>mdi-plus</v-icon></v-btn>
         </template>
         <v-card max-width="700" flat>
-        <v-card-title class="heading-1 blue lighten-4 primary--text">Staff ID - {{staff.staffID}}
-            <v-spacer></v-spacer>
-            <v-btn depressed color="primary" dark @click="isEditing = !isEditing" v-if="!isEditing"> Edit
-                <v-icon right>mdi-account-edit</v-icon>
-            </v-btn>  
-        </v-card-title>
+        <v-card-title class="heading-1 blue lighten-4 primary--text">Create Subject</v-card-title>
         
         <v-divider></v-divider>
         <v-card-text style="height: 800px;">
-            <v-form ref="form" v-model="valid" lazy-validation class="px-5" :readonly="!isEditing">
-                <v-card-text class="grey--text">Staff Details</v-card-text>
-                <v-responsive class="text-center">
-                    <v-avatar  size="150" class="mb-2">
-                        <v-icon size="150" v-if="!imageUrl">mdi-account-circle</v-icon>
-                        <img :src="imageUrl" v-if="imageUrl">
-                    </v-avatar> <br>
-                    <v-btn @click="onPickFile" depressed class="white grey--text" v-if="isEditing">
-                        <v-icon left>mdi-camera</v-icon>
-                        <span>upload image</span>
-                    </v-btn>
-                </v-responsive>
-                <input type="file" v-show="false" ref="fileInput" accept="image/*" @change="onFilePicked">
+            <v-form ref="form" v-model="valid" lazy-validation>
                 
+                <fieldset class="px-5 pb-3">
+                    <legend><v-card-text class="grey--text">Subject Details</v-card-text></legend>
+                    <v-row justify="center" dense >
+                            <v-col cols="12" md="6" sm="6">
+                                <v-text-field :rules="subjectRules" label="Subject" prepend-icon="mdi-format-align-center" v-model="subject"></v-text-field>
+                            </v-col>
 
-                <v-row justify="center" dense >
+                            <v-col cols="12" md="6" sm="6">
+                                <v-select :items="medium" v-model="getMedium" :rules="mediumRules" label="Medium" prepend-icon="mdi-web" required></v-select>
+                            </v-col>
+                    </v-row>
+                </fieldset>
+                <fieldset class="px-5 pb-3">
+                    <legend><v-card-text class="grey--text">Category Details</v-card-text></legend>
 
-                    <v-col cols="12" md="12" sm="12">
-                        <v-select :items="title" :rules="titleRules" label="Title" prepend-icon="mdi-badge-account" v-model="getTitle"></v-select>
-                    </v-col>
-
-                    <v-col cols="12" md="6" sm="6">
-                        <v-text-field v-model="fname" :rules="nameRules" label="First Name" prepend-icon="mdi-account" required></v-text-field>
-                    </v-col>
-
-                    <v-col cols="12" md="6" sm="6">
-                        <v-text-field v-model="lname" :rules="nameRules" label="Last Name" prepend-icon="mdi-account" required></v-text-field>
-                    </v-col>
-
-                    <v-col cols="12" md="6" sm="6" >
-                        <v-radio-group v-model="nicType" row style="justify-content:center !important">
-                            <v-radio label="Old NIC" value="old"></v-radio>
-                            <v-radio label="New NIC" value="new"></v-radio>
-                        </v-radio-group>
-                    </v-col>
-
-                    <v-col cols="12" md="6" sm="6">
-                        <v-text-field v-if="nicType=='new'" v-model="nicNo" :rules="newNicRules" label="NIC no.new" prepend-icon="mdi-card-bulleted" required maxlength="12"></v-text-field>
-                        <v-text-field v-else-if="nicType=='old'" v-model="nicNo" :rules="oldNicRules" label="NIC no.old" prepend-icon="mdi-card-bulleted" required maxlength="10"></v-text-field>
-                    </v-col>
-
-                    <v-col cols="12" md="6" sm="6">
-                        <v-select :items="gender" :rules="genderRules" label="Gender" prepend-icon="mdi-gender-male-female" v-model="getGender"></v-select>
-                    </v-col>
-
-                    <v-col cols="12" md="6" sm="6">
-                        <template>
-                            <div>
-                                <v-menu ref="menu" v-model="menu" :close-on-content-click="false" transition="scale-transition" offset-y min-width="auto" :disabled="!isEditing">
-                                    <template v-slot:activator="{ on, attrs }">
-                                        <v-text-field v-model="date" label="Birthday date" prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on" :rules="birthdayRules"></v-text-field>
-                                    </template>
-                                    <v-date-picker v-model="date" :active-picker.sync="activePicker" :max="(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)" min="1950-01-01" ></v-date-picker>
-                                </v-menu>
-                            </div>
-                        </template>
-                    </v-col>
-
-                    <v-col cols="12" md="6" sm="6">
-                        <v-text-field v-model="tp" :rules="tpRules" label="Telephone No." prepend-icon="mdi-phone-classic" required maxlength="10" ></v-text-field>
-                    </v-col>
-
-                    <v-col cols="12" md="6" sm="6">
-                        <v-text-field v-model="email"  :rules="emailRules" label="E-mail" prepend-icon="mdi-mail" required></v-text-field>
-                    </v-col>
-
-                    <v-col cols="12"  md="12" sm="12">
-                        <v-text-field v-model="address"  :rules="addressRules" label="Address" prepend-icon="mdi-map-marker" required></v-text-field>
-                    </v-col>
-
-                    <v-card-text class="grey--text">Class Details</v-card-text>
-
-                    <v-col cols="12" md="6" sm="6">
-                        <v-select :items="brach" :rules="branchRules" label="Branch" prepend-icon="mdi-sitemap" v-model="getBrach"></v-select>
-                    </v-col>
-
-                    <v-col cols="12" md="6" sm="6">
-                        <template>
-                            <div>
-                                <v-menu ref="menud" v-model="joingMenu" :close-on-content-click="false" transition="scale-transition" offset-y min-width="auto" :disabled="!isEditing">
-                                    <template v-slot:activator="{ on, attrs }">
-                                        <v-text-field v-model="joingDate" label="Join date" prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on" :rules="joinDateRules"></v-text-field>
-                                    </template>
-                                    <v-date-picker v-model="joingDate" :active-picker.sync="joingActivePicker" :max="(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)" min="1950-01-01"></v-date-picker>
-                                </v-menu>
-                            </div>
-                        </template>
-                    </v-col>
                     
-                </v-row>
+                    <div :hidden="hideTable">
+                        <v-col cols="12" md="12" sm="12">
+                            <v-text-field :rules="categoryRules" label="Category" prepend-icon="mdi-candy-outline" v-model="categoryName" readonly hint="Add category from below table"></v-text-field>
+                            <v-card-text class="grey--text">
+                                * Add category from below table
+                            </v-card-text>
+                        </v-col>
+                        <v-card-title><v-spacer></v-spacer><v-text-field v-model="search" append-icon="mdi-magnify" label="Search Category" single-line hide-details></v-text-field></v-card-title>
+                        <v-data-table :headers="headers" :items="categories" :search="search" :items-per-page="5">
+                            <template v-slot:item="row">
+                                <tr>
+                                    <td>{{row.item.name}}</td>
+                                    <td>
+                                        <v-btn @click="category=row.item.id, categoryName=row.item.name" depressed color="primary" outlined>Add</v-btn>
+                                    </td>
+                                </tr>
+                            </template>
+                        </v-data-table>
+                        <v-divider></v-divider>
+                        <v-row>
+                            <v-card-text class="grey--text">
+                                * If you won't find the category from the table, 
+                            </v-card-text>
+                            <v-col class="text-center my-3">
+                                <v-btn color="primary" depressed @click="hideTable= !hideTable" outlined>create a new category</v-btn>
+                            </v-col>
+                        </v-row>
+                    </div>
+                    
+                    
+                    
+                    
+                    <div :hidden="!hideTable">
+                        <v-card-text class="grey--text">
+                            * Create new Category
+                        </v-card-text>
+                        <v-row>
+                            <v-col cols="12" md="6" sm="6">
+                                <v-text-field label="New Category" prepend-icon="mdi-candy-outline" v-model="newCategory" :error-messages="errormsg"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" md="6" sm="6" align-self="center">
+                                <v-btn depressed color="primary" outlined @click="createCategory()">Create Category</v-btn>
+                                <v-btn outlined color="grey" class="ml-2" @click="hideTable = !hideTable">Cancel</v-btn>
+                            </v-col>
+                        </v-row>
+                    </div>
+                    
+                    
+                </fieldset>
             </v-form>
             
           
@@ -116,16 +88,14 @@
 
         <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn   @click="dialog = false" color="primary" v-if="!isEditing" depressed>OK</v-btn>
-            <v-btn   @click="cancelEdit()" outlined color="grey" v-if="isEditing">Cancel</v-btn>
-            <v-btn :disabled="!valid" color="primary" @click="Save()" depressed v-if="isEditing">Save
-                <v-icon left>mdi-content-save</v-icon>
-            </v-btn>
+            <v-btn   @click="dialog = false, failedAlert()" outlined color="grey">Cancel</v-btn>
+            <v-btn :disabled="!valid || !subject || !getMedium || !categoryName" color="primary" @click="Save(), successAlert(), dialog = false" depressed>Create</v-btn>
         </v-card-actions>
 
-        <v-snackbar v-model="hasSaved" :timeout="2000" absolute bottom left color="green">Staff details has been updated</v-snackbar>
-
+        
+        <v-snackbar v-model="categoryCreated" :timeout="2000" absolute bottom left color="green">Category has been created</v-snackbar>
       </v-card>
+      
     </v-dialog>
   </v-row>
 </template>
@@ -134,69 +104,49 @@
 
 <script>
 export default {
-    props:['staff'],
+    
     data(){
         return{
-            imageUrl:'',
-            image:null,
-
-            valid:true,
-            fname: this.staff.fname,
-            lname: this.staff.lname,
-            tp: this.staff.tp,
-            email: this.staff.email,
-            address: this.staff.address,
-            getGender:this.staff.getGender,
-            nicNo:this.staff.nicNo,
-            nicType:this.staff.nicType,
-            getTitle:this.staff.getTitle,
-
-            activePicker: null,
-            date: this.staff.date,
-            getBrach:this.staff.getBrach,
-
-            joingActivePicker: null,
-            joingDate: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
-            joingMenu: false,
-
-            menu: false,
-
             dialogm1: '',
             dialog: false,
 
-            isEditing: null,
-            hasSaved: false,
+            valid:true,
+
+            subject:'',
+            getMedium:'Sinhala',
+            category:'',
+            categoryName:'',
+            newCategory:'',
+
+            errormsg:null,
+            categoryCreated:false,
+
+            hideTable:false,
+
+
+            search: '',
+            headers: [
+                { text: 'Category',align: 'start', sortable: false, value:'name'},
+                { text: '', sortable: false, value: 'id',align:'right'},
+            ],
+
+            categories: [
+                {name:'Ordinary Level',id:'cat001'},
+                {name:'Advanced Level',id:'cat002'},
+                {name:'Scholarship',id:'cat003'},
+                {name:'Professional',id:'cat004'}
+            ],
             
             // -----------Validation rules-----------
-            nameRules: [v=> !!v || 'Name is required', v => /^[a-zA-Z_ ]*$/.test(v) || 'Must be text only', v=> (v && v.length >3)|| 'Name must be greater than 3'],
-            
-            tpRules: [v=> !!v || 'Telephone no. is required', v => /^\d+$/.test(v) || 'Must be a number', v=> (v && v.length ==10)|| 'Telephone no. must be 10'],
-            
-            emailRules: [v=> !!v || 'email is required', v=> /.+@.+\..+/.test(v) || 'email must be valid'],         
-            
-            addressRules: [v=> !!v || 'Address is required', v=> (v && v.length >5)|| 'Address must be greater than 5'],
+            subjectRules: [v=> !!v || 'Subject is required', v => /^[a-zA-Z_ ]*$/.test(v) || 'Must be text only', v=> (v && v.length >2)|| 'Name must be greater than 2'],
 
-            birthdayRules: [v=> !!v || 'Birthday is required'],
+            mediumRules: [v=> !!v || 'Medium is required'],
 
-            genderRules: [v=> !!v || 'Gender is required'],
-
-            newNicRules: [v=> !!v || 'NIC is required', v => /^\d+$/.test(v) || 'Must be a number', v=> (v && v.length ==12)|| 'New Nic no. must have 12 numbers'],
-            
-            oldNicRules: [v=> !!v || 'NIC is required', v => /[0-9]+[vxVX]/.test(v) || 'Please insert valid NIC', v=> (v && v.length ==10)|| 'Old Nic no. must have 10 characters'],
-
-            branchRules: [v=> !!v || 'Branch is required'],
-
-            joinDateRules: [v=> !!v || 'Join Date is required'],
-
-            titleRules: [v=> !!v || 'Title is required'],
+            categoryRules: [v=> !!v || 'Category is required'],
 
 
             // -----------dropdown list-----------
-            gender:['Male','Female','Other'],
-
-            brach:['Hakmana','Walasmulla'],
-
-            title:['Mr', 'Ms', 'Mrs', 'Miss']
+            medium:['Sinhala', 'English', 'Tamil']
 
         
         }
@@ -214,34 +164,48 @@ export default {
             if(this.$refs.form.validate()){
                 console.log('fname:'+this.fname+' lname:'+this.lname+' tp:'+this.tp+' email:'+this.email+' address:'+this.address+' bday:'+this.date+' gender:'+this.getGender);
                 
-                this.isEditing = !this.isEditing;
-                this.hasSaved = true;
             }  
         },
 
-        onPickFile(){
-            this.$refs.fileInput.click();
-        },
-        onFilePicked(event){
-            const files = event.target.files
-            let filename = files[0].name
-            if(filename.lastIndexOf('.')<=0){
-                return alert('please add a valid file!!!')
+        createCategory(){
+            var rule=/^[a-zA-Z_ ]*$/;
+            if(!rule.test(this.newCategory)){
+                this.errormsg="Must be text only"
+            }else if(this.newCategory.length<3){
+                this.errormsg="Name must be greater than 2"
+            }else{
+                this.errormsg=null
+                console.log(this.newCategory)
+                this.categoryCreated=true
+                this.hideTable=false
             }
-            const fileReader = new FileReader()
-            fileReader.addEventListener('load', () => {
-            this.imageUrl = fileReader.result
-            })
-            fileReader.readAsDataURL(files[0])
-            this.image = files[0]
         },
-        
-        cancelEdit(){
-            this.isEditing = !this.isEditing;
-            this.hasSaved = false;
+
+        successAlert(){
+                this.$emit('success',true)
+        },
+        failedAlert(){
+            this.$emit('failed',true)
         }
+
+       
+
+        
+        
+        
         
       
     }
 }
 </script>
+
+
+<style scoped>
+    fieldset{
+        border-color: rgb(225, 225, 225);
+        border-style: solid;
+        border:0.1;
+        border-style: solid;
+    }
+
+</style>
--- src/components/Subjects.vue
+++ src/components/Subjects.vue
@@ -8,38 +8,71 @@
             </v-breadcrumbs>
         <v-container>
 
+            <!----------------------------- Alerts ---------------------------------->
+
             <v-snackbar :timeout="3000" v-model="unsuccessAlert" color="red"  bottom ><v-icon left>mdi-alert-outline</v-icon> Student delete <strong>failed</strong> </v-snackbar>
             <v-snackbar :timeout="3000" v-model="successAlert" color="green"  bottom><v-icon left>mdi-check</v-icon>Student delete <strong>successful</strong> </v-snackbar>
             
+            <v-snackbar :timeout="3000" v-model="unsuccessAlertSubjectCreate" color="red"  bottom ><v-icon left>mdi-alert-outline</v-icon>Subject Create <strong>failed</strong> </v-snackbar>
+            <v-snackbar :timeout="3000" v-model="successAlertSubjectCreate" color="green"  bottom><v-icon left>mdi-check</v-icon>Subject Create <strong>successful</strong> </v-snackbar>
             
             
-            
-
+            <!----------------------------- Alerts ---------------------------------->
             
 
             <template>
-                <v-card flat>
-                    <v-card-title class="heading-1 blue lighten-4 primary--text">Subjects</v-card-title>
-                    <v-card-title><v-spacer></v-spacer><v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field></v-card-title>
-                    
-                    <v-data-table :headers="headers" :items="subjects" :search="search">
-                        <template v-slot:item="row">
-                            <tr>
-                                <td>{{row.item.name}}</td>
-                                <td>{{row.item.id}}</td>
-                                <td>{{row.item.medium}}</td>
-                                <td>{{row.item.category}}</td>
-                                <td >
-                                    <app-ViewStudentDetails :student='row.item'></app-ViewStudentDetails>
-                                </td>
-                                <td>
-                                    <app-DeleteStudent :student='row.item' @success="deleteAlert($event)" @failed="faileAlert($event)"></app-DeleteStudent>
-                                </td>
-                                <td></td>
-                            </tr>
-                        </template>
-                    </v-data-table>
-                </v-card>
+                <v-row>
+                    <v-col>
+                        <v-card flat>
+                            <v-card-title class="heading-1 blue lighten-4 primary--text ">Subjects
+                                <v-spacer></v-spacer>
+                                <app-CreateSubject class="mr-2" @success="subjectCreateSuccessAlert($event)" @failed="subjectCreateFaileAlert($event)"></app-CreateSubject>
+                            </v-card-title>
+                            <v-card-title><v-spacer></v-spacer><v-text-field v-model="subjectSearch" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field></v-card-title>
+                            
+                            <v-data-table :headers="subjectHeaders" :items="subjects" :search="subjectSearch">
+                                <template v-slot:item="row">
+                                    <tr>
+                                        <td>{{row.item.name}}</td>
+                                        <td>{{row.item.id}}</td>
+                                        <td>{{row.item.medium}}</td>
+                                        <td>{{row.item.category}}</td>
+                                        <td >
+                                            <app-ViewStudentDetails :student='row.item'></app-ViewStudentDetails>
+                                        </td>
+                                        <td>
+                                            <app-DeleteStudent :student='row.item' @success="deleteAlert($event)" @failed="faileAlert($event)"></app-DeleteStudent>
+                                        </td>
+                                        
+                                    </tr>
+                                </template>
+                            </v-data-table>
+                        </v-card>
+                    </v-col>
+                    <v-col>
+                        <v-card flat>
+                            <v-card-title class="heading-1 blue lighten-4 primary--text ">Category</v-card-title>
+                            <v-card-title><v-spacer></v-spacer><v-text-field v-model="categorySearch" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field></v-card-title>
+                            
+                            <v-data-table :headers="categoryHeaders" :items="categories" :search="categorySearch">
+                                <template v-slot:item="row">
+                                    <tr>
+                                        <td>{{row.item.name}}</td>
+                                        <td>{{row.item.id}}</td>
+                                        <td >
+                                            <app-ViewStudentDetails :student='row.item'></app-ViewStudentDetails>
+                                        </td>
+                                        <td>
+                                            <app-DeleteStudent :student='row.item' @success="deleteAlert($event)" @failed="faileAlert($event)"></app-DeleteStudent>
+                                        </td>
+                                        
+                                    </tr>
+                                </template>
+                            </v-data-table>
+                        </v-card>
+                    </v-col>
+                </v-row>
+                
             </template>
 
             
@@ -53,32 +86,47 @@
 <script>
     import ViewStudentDetails from './ViewStudentDetails.vue'
     import DeleteStudent from './DeleteStudent.vue'
+    import CreateSubject from './CreateSubject.vue'
 
     export default {
         components:{
             'app-ViewStudentDetails':ViewStudentDetails,
-            'app-DeleteStudent':DeleteStudent
+            'app-DeleteStudent':DeleteStudent,
+            'app-CreateSubject':CreateSubject
         },
         data () {
             return {
-                search: '',
-                headers: [
+                subjectSearch: '',
+                categorySearch: '',
+                subjectHeaders: [
                     {text: 'NAME',align: 'start', sortable: true, value:'name'},
                     { text: 'ID', sortable: false, value: 'id' },
-                    { text: 'MEDIUM',sortable: true, value: 'medium' },
+                    { text: 'MEDIUM',sortable: true, value: 'medium', filterable:false},
                     { text: 'CATEGORY', sortable: true, value: 'category' },
                     { text: '', sortable: false, value: 'Action' },
                     { text: '', sortable: false, value: 'Action' },
                 ],
 
                 subjects: [
-                    {name:'Science',id:'sub001',medium:'Sinhala',category:'OL'},
-                    {name:'Maths',id:'sub002',medium:'Sinhala',category:'OL'},
+                    {name:'Science',id:'sub001',medium:'Sinhala',category:'Ordinary Level'},
+                    {name:'Maths',id:'sub002',medium:'Sinhala',category:'Ordinary Level'},
                     {name:'Scholarship',id:'sub003',medium:'Sinhala',category:'Scholarship'},
-                    {name:'BS',id:'sub004',medium:'Sinhala',category:'AL'},
-                    {name:'Maths',id:'sub005',medium:'English',category:'OL'}
-                    
-                    
+                    {name:'BS',id:'sub004',medium:'Sinhala',category:'Advanced Level'},
+                    {name:'Maths',id:'sub005',medium:'English',category:'Ordinary Level'}
+                ],
+
+                categoryHeaders: [
+                    {text: 'NAME',align: 'start', sortable: true, value:'name'},
+                    { text: 'ID', sortable: false, value: 'id' },
+                    { text: '', sortable: false, value: 'Action' },
+                    { text: '', sortable: false, value: 'Action' },
+                ],
+
+                categories: [
+                    {name:'Ordinary Level',id:'cat001'},
+                    {name:'Advanced Level',id:'cat002'},
+                    {name:'Scholarship',id:'cat003'},
+                    {name:'Professional',id:'cat004'}
                 ],
 
                 breadcrumbs: [
@@ -88,6 +136,10 @@
 
                 successAlert:false,
                 unsuccessAlert:false,
+
+                unsuccessAlertSubjectCreate:false,
+                successAlertSubjectCreate:false,
+
             }
         },
 
@@ -97,6 +149,13 @@
             },
             faileAlert(failed){
                 this.unsuccessAlert = failed;
+            },
+
+            subjectCreateSuccessAlert(success){
+                this.successAlertSubjectCreate = success;
+            },
+            subjectCreateFaileAlert(failed){
+                this.unsuccessAlertSubjectCreate = failed;
             },
         }
     }
