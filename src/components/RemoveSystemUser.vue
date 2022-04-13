0a8784e2e1be72e005817b0abd682be42ad1abf1
pragith98 <lakshanxp@hotmail.com>
2022-04-14 00:22:27 +0530

    complete remove system user component

---
+++ src/components/RemoveSystemUser.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-row justify="start">
+        <v-dialog v-model="dialog" persistent max-width="400px">
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn class="error" fab dark x-small depressed v-bind="attrs" v-on="on"><v-icon dark>mdi-close</v-icon></v-btn>
+            </template>
+            <v-card>
+                <v-container class="text-center">
+                    <v-card-title>
+                        <v-row justify="center">
+                            <v-icon size="100" color="error">mdi-help-circle-outline</v-icon>
+                        </v-row>
+                    </v-card-title>
+                    <span class="text-h6 text-center">Do you really want to remove <br> <strong>"{{staff.fname}}"</strong></span>
+                </v-container>
+                
+                
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="grey" @click="dialog = false, failedAlert()" outlined>Cancel</v-btn>
+                    <v-btn color="error" @click="dialog = false, successAlert()" depressed>Remove</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-row>
+</template>
+
+
+
+<script>
+    export default {
+        props:['staff'],
+        data: () => ({
+            dialog: false,
+            
+        }),
+
+        methods:{
+            successAlert(){
+                this.$emit('success',true)
+            },
+            failedAlert(){
+                this.$emit('failed',true)
+            }
+        }
+    }
+</script>
--- src/components/SystemUsers.vue
+++ src/components/SystemUsers.vue
@@ -8,8 +8,8 @@
             </v-breadcrumbs>
         <v-container>
 
-            <v-snackbar :timeout="3000" v-model="unsuccessAlert" color="red"  bottom ><v-icon left>mdi-alert-outline</v-icon> Staff delete <strong>failed</strong> </v-snackbar>
-            <v-snackbar :timeout="3000" v-model="successAlert" color="green"  bottom><v-icon left>mdi-check</v-icon>Staff delete <strong>successful</strong> </v-snackbar>
+            <v-snackbar :timeout="3000" v-model="unsuccessAlert" color="red"  bottom ><v-icon left>mdi-alert-outline</v-icon>Account remove <strong>failed</strong> </v-snackbar>
+            <v-snackbar :timeout="3000" v-model="successAlert" color="green"  bottom><v-icon left>mdi-check</v-icon>Account remove <strong>successful</strong> </v-snackbar>
             
             
  
@@ -18,19 +18,25 @@
                 <v-card flat>
                     <v-card-title class="heading-1 blue lighten-4 primary--text">System Users</v-card-title>
                     <v-card-title><v-spacer></v-spacer><v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field></v-card-title>
-                    <!-- <v-data-table :headers="headers" :items="desserts" :search="search" ></v-data-table> -->
                     <v-data-table :headers="headers" :items="staff" :search="search">
                         <template v-slot:item="row">
                             <tr>
                                 <td>{{row.item.staffID}}</td>
                                 <td>{{row.item.fname}} {{row.item.lname}}</td>
+                                <td>
+                                    <v-chip small :class="row.item.role" text-color="white">
+                                        {{row.item.role}}
+                                    </v-chip>
+                                </td>
                                 <td>{{row.item.tp}}</td>
-                                <td>{{row.item.getBrach}}</td>
+                                <td>
+                                    <v-chip outlined small>{{row.item.getBrach}}</v-chip>
+                                </td>
                                 <td >
                                     <app-ViewStaffDetails :staff='row.item'></app-ViewStaffDetails>
                                 </td>
                                 <td>
-                                    <app-DeleteStaff :staff='row.item' @success="deleteAlert($event)" @failed="faileAlert($event)"></app-DeleteStaff>
+                                    <app-RemoveSystemUser :staff='row.item' @success="deleteAlert($event)" @failed="faileAlert($event)"></app-RemoveSystemUser>
                                 </td>
                                 <td></td>
                             </tr>
@@ -49,21 +55,23 @@
 
 <script>
     import ViewStaffDetails from './ViewStaffDetails.vue'
-    import DeleteStaff from './DeleteStaff.vue'
+    import RemoveSystemUser from './RemoveSystemUser.vue'
 
     export default {
         components:{
             'app-ViewStaffDetails':ViewStaffDetails,
-            'app-DeleteStaff':DeleteStaff
+            'app-RemoveSystemUser':RemoveSystemUser
         },
         data () {
             return {
                 search: '',
                 headers: [
-                    { text: 'STAFF ID', sortable: false, value: 'staffID' },
+                    { text: 'ID', sortable: false, value: 'staffID' },
                     { text: 'NAME',align: 'start', sortable: false, value:'fname'},
+                    { text: 'ROLE', sortable: true, value: 'role' },
                     { text: 'TELEPHONE NO.', sortable: false, value: 'tp' },
                     { text: 'BRANCH', sortable: true, value: 'getBrach' },
+                    
 
                     { text: '', sortable: false, value: 'Action' },
                     { text: '', sortable: false, value: 'Action' },
@@ -71,10 +79,10 @@
                 ],
 
                 staff: [
-                    {fname:'Saman', lname:'Herath', nicType:'old', nicNo:'871982289v', tp:'1231235323', email:'Saman@Saman.com', address:'no1, rathnapura', getGender:'Male', staffID:'2621', date:'2021-02-17', getBrach:'Hakmana'},
-                    {fname:'Dasun', lname:'Rathnayake', nicType:'old', nicNo:'871982289v', tp:'4321235323', email:'Dasun@Dasun.com', address:'no1, Matale', getGender:'Male', staffID:'2065', date:'2021-07-19', getBrach:'Walasmulla'},
-                    {fname:'Kasun', lname:'Bandara', nicType:'old', nicNo:'871982289v', tp:'7831235323', email:'Kasun@Kasun.com', address:'no1, Kandy', getGender:'Male', date:'2021-02-19', staffID:'2071', getBrach:'Kamburupitiya'},
-                    {fname:'Maheshi', lname:'Ranathunga', nicType:'old', nicNo:'871982289v',tp:'9931235323', email:'Maheshi@Maheshi.com', address:'no1, Jafna', getGender:'Female', date:'2021-09-12', staffID:'2024', getBrach:'Walasmulla'},
+                    {fname:'Saman', lname:'Herath', nicType:'old', nicNo:'871982289v', tp:'1231235323', email:'Saman@Saman.com', address:'no1, rathnapura', getGender:'Male', staffID:'2621', date:'2021-02-17', getBrach:'Hakmana', role:'Teacher'},
+                    {fname:'Dasun', lname:'Rathnayake', nicType:'old', nicNo:'871982289v', tp:'4321235323', email:'Dasun@Dasun.com', address:'no1, Matale', getGender:'Male', staffID:'2065', date:'2021-07-19', getBrach:'Walasmulla', role:'Admin'},
+                    {fname:'Kasun', lname:'Bandara', nicType:'old', nicNo:'871982289v', tp:'7831235323', email:'Kasun@Kasun.com', address:'no1, Kandy', getGender:'Male', date:'2021-02-19', staffID:'2071', getBrach:'Kamburupitiya', role:'Standerd user'},
+                    {fname:'Maheshi', lname:'Ranathunga', nicType:'old', nicNo:'871982289v',tp:'9931235323', email:'Maheshi@Maheshi.com', address:'no1, Jafna', getGender:'Female', date:'2021-09-12', staffID:'2024', getBrach:'Walasmulla', role:'Standerd user'},
                     
                 ],
 
@@ -102,7 +110,18 @@
 
 
 
+<style scoped>
 
+    .theme--light.v-chip:not(.v-chip--active).v-chip.Teacher{
+        background: orange;
+    }
+    .theme--light.v-chip:not(.v-chip--active).v-chip.Admin{
+        background: rgb(0, 192, 0);
+    }
+    .theme--light.v-chip:not(.v-chip--active).v-chip.Standerd{
+        background: rgb(185, 182, 182);
+    }
+</style>
 
             
         
