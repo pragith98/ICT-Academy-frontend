ea00f2cb036bc6548e86144dd5cd34925edcecf9
pragith98 <lakshanxp@hotmail.com>
2022-07-01 14:15:20 +0530

    set funtions to get deactivated staff details

--- src/components/DeactivatedStaffDetails.vue
+++ src/components/DeactivatedStaffDetails.vue
@@ -8,23 +8,24 @@
             </v-breadcrumbs>
         <v-container>
 
-            <v-snackbar :timeout="3000" v-model="unsuccessAlert" color="red"  bottom ><v-icon left>mdi-alert-outline</v-icon> Teacher delete <strong>failed</strong> </v-snackbar>
-            <v-snackbar :timeout="3000" v-model="successAlert" color="green"  bottom><v-icon left>mdi-check</v-icon>Teacher delete <strong>successful</strong> </v-snackbar>
+            <v-snackbar :timeout="3000" v-model="unsuccessAlert" color="red"  bottom ><v-icon left>mdi-alert-outline</v-icon> Staff delete <strong>failed</strong> </v-snackbar>
+            <v-snackbar :timeout="3000" v-model="successAlert" color="green"  bottom><v-icon left>mdi-check</v-icon>Staff delete <strong>successful</strong> </v-snackbar>
             
-          
+            
+ 
 
             <template>
                 <v-card flat>
-                    <v-card-title class="heading-1 blue-grey lighten-4  blue-grey--text text--darken-2">Deactivated Teacher Details</v-card-title>
+                    <v-card-title class="heading-1 blue-grey lighten-4  blue-grey--text text--darken-2">Deativated Staff Details</v-card-title>
                     <v-card-title><v-spacer></v-spacer><v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field></v-card-title>
                     
-                    <v-data-table :headers="headers" :items="teachers" :search="search">
+                    <v-data-table :headers="headers" :items="staff" :search="search">
                         <template v-slot:[`item.actions`]="{ item }">
                             <v-card-actions>
-                                <app-ViewTeacherDetails @success="reCreate($event)" :teacher='item'></app-ViewTeacherDetails>
+                                <app-ViewStaffDetails @success="reCreate($event)" :staff='item'></app-ViewStaffDetails>
                                 <v-spacer></v-spacer>
-                                <app-DeleteTeacher class="ml-5" :teacher='item' @success="deleteAlert($event)" @failed="faileAlert($event)"></app-DeleteTeacher>
-                            </v-card-actions> 
+                                <app-DeleteStaff class="ml-5" :staff='item' @success="deleteAlert($event)" @failed="faileAlert($event)"></app-DeleteStaff>
+                            </v-card-actions>
                         </template>
                     </v-data-table>
                 </v-card>
@@ -39,13 +40,13 @@
 </template>
 
 <script>
-    import ViewTeacherDetails from './ViewTeacherDetails.vue'
-    import DeleteTeacher from './DeleteTeacher.vue'
+    import ViewStaffDetails from './ViewStaffDetails.vue'
+    import DeleteStaff from './DeleteStaff.vue'
 
     export default {
         components:{
-            'app-ViewTeacherDetails':ViewTeacherDetails,
-            'app-DeleteTeacher':DeleteTeacher
+            'app-ViewStaffDetails':ViewStaffDetails,
+            'app-DeleteStaff':DeleteStaff
         },
         data () {
             return {
@@ -54,14 +55,15 @@
                     { text: 'NAME',align: 'start', sortable: false, value:'firstName'},
                     { text: 'TELEPHONE NO.', sortable: false, value: 'telNo' },
                     { text: 'EMAIL', sortable: false, value: 'email' },
+                    { text: 'BRANCH', sortable: true, value: 'branch.branchName' },
                     { text: '', sortable: false, value: 'actions' },
                 ],
 
-                teachers:[],
+                staff: [],
 
                 breadcrumbs: [
-                    { text: 'Teachers', disabled: false, href: '/Teachers' },
-                    { text: 'DeactivatedTeacherDetails', disabled: true, href: '/Teachers/DeactivatedTeacherDetails' }
+                    { text: 'Staff', disabled: false, href: '/Staff' },
+                    { text: 'DeativatedStaffDetails', disabled: true, href: '/Staff/DeativatedStaffDetails' }
                 ],
 
                 successAlert:false,
@@ -70,12 +72,12 @@
         },
 
         created(){
-            this.getTeachers()
+            this.getStaff()
         },
 
         methods: {
             deleteAlert(success){
-                this.getTeachers();
+                this.getStaff()
                 this.successAlert = success;
             },
 
@@ -83,27 +85,28 @@
                 this.unsuccessAlert = failed;
             },
 
-            getTeachers(){
-                this.axios.get(this.$apiUrl+"/api/v1.0/TeacherManagement/teachers",{
+            getStaff(){
+                this.axios.get(this.$apiUrl+"/api/v1.0/StaffManagement/staffs",{
                 params:{
                     status: "Deactivate"
                 }
                 
                 }).then(Response=>(
-                    this.teachers=Response.data.teacher.data,
-                    
-                    this.teachers.forEach(element => {
+                    this.staff=Response.data.staff.data,
+
+                    this.staff.forEach(element => {
                         element.firstName=element.title+" "+element.firstName+" "+element.lastName
                     })
-                    
                 ))
             },
 
+
             reCreate(success){
-                this.getTeachers();
+                this.getStaff();
                 console.log(success)
 
             },
+
         }
     }
 </script>
