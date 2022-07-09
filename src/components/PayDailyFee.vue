eb24317f02ca75f21fd16b3d07ab75950b4e969b
pragith98 <lakshanxp@hotmail.com>
2022-07-10 00:21:14 +0530

    change ui

--- src/components/PayDailyFee.vue
+++ src/components/PayDailyFee.vue
@@ -1,80 +1,50 @@
 <template>
   <v-row justify="end">
-    <v-dialog v-model="dialog" scrollable max-width="700px" persistent>
+    <v-dialog v-model="dialog" scrollable max-width="450" persistent>
         <template v-slot:activator="{ on, attrs }">
-           <v-btn block class="teal" dark depressed  v-bind="attrs" v-on="on">Pay<v-icon dark right>mdi-cash-multiple</v-icon></v-btn>
+           <v-btn @click="selectedPayments=null" width="200" block class="teal" dark depressed  v-bind="attrs" v-on="on">Pay<v-icon dark right>mdi-cash-multiple</v-icon></v-btn>
         </template>
-        <v-card max-width="700" flat >
+        <v-card max-width="450" flat >
             <v-card-title class="heading-1 blue-grey lighten-4  blue-grey--text text--darken-2">Payment</v-card-title>
             
             <v-divider></v-divider>
             <v-card-text >
-                <v-form ref="form" v-model="valid" lazy-validation >
-                    <fieldset class="px-15 py-5 my-3">
-                        <legend>Payment Details</legend>
-                        <v-simple-table dense>
-                            <thead>
-                                <tr>
-                                    <th class="text-left" colspan="2">
-                                        <h2>Class Fee</h2>
-                                    </th>
-                                    <th class="text-right">
-                                        <h2>Rs. {{classFee}}.00</h2> 
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <h3>Day Count</h3>
-                                    </td>
-                                    <td class="text-right">
-                                        <h3>{{dayCount}} x</h3>
-                                    </td>
-                                    <td class="text-right">
-                                        <h3>Rs. {{total}}.00</h3>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <h2>Total</h2>
-                                    </td>
-                                    <td class="text-right">
-                                        <h2></h2>
-                                    </td>
-                                    <td class="text-right">
-                                        <v-divider></v-divider>
-                                        <h2>Rs. {{total}}.00</h2>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </v-simple-table>
-                    </fieldset>
-                    
-                    <fieldset class="pa-15">
-                        <legend>Pay</legend>
-                        <v-row>
-                            <v-col>
-                                <v-text-field :rules="amountRules" label="Amount (Rs.)" v-model="getTotal"></v-text-field>
-                            </v-col>
-                            <v-col align-self="center">
-                                <v-btn block outlined color="primary" @click="markPaid(getTotal)" :disabled="!valid || !getTotal">Paid</v-btn>
-                            </v-col>
-                        </v-row>
-                    </fieldset>
+                <v-form ref="form" v-model="valid" lazy-validation>
+                    <div>
+                        <v-card-text>This is <strong>{{student.studentName}}</strong>'s payment details. You can mark payment for <strong>Arrears/Today fee</strong>. And also you can <strong>change values</strong> before payment.</v-card-text>
+                        <template>
+                            <div>
+                                <div class="pa-5">
+                                    <legend> Payment Details </legend>
+                                    <v-data-table @input="getSelect($event)" :headers="headers" :items="payments" show-select item-key="no" dense hide-default-footer>
+                                        <template v-slot:[`item.classFee`]="{ item }" >
+                                            <v-text-field :rules="amountRules" class="centered-input" suffix=".00"  style="width:70px" v-model="item.fee" dense></v-text-field>
+                                        </template>
+                                    </v-data-table>
+                                </div>
+                                <v-divider></v-divider>
+                                <v-row class="mt-3">
+                                    <v-col class="text-right">
+                                        <v-card-text class="text-h6">Amount (Rs.)</v-card-text>
+                                    </v-col>
+                                    <v-col>
+                                        <v-text-field readonly class="centered-input text-h5" v-model="amount" style="width:150px" outlined ></v-text-field>
+                                    </v-col>
+                                </v-row>
+                            </div>
+                            
+                        </template>
+                        
+                    </div>
                 </v-form>
+               
             </v-card-text>
             <v-divider></v-divider>
-
-
-
-
             <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn  color="primary" @click="dialog = false" depressed>OK</v-btn>
+                <v-btn color="grey" @click="dialog = false" outlined>Cancel</v-btn>
+                <v-btn  color="primary" @click="dialog = false" depressed width="150" :disabled="!valid || selectedPayments=='' || !selectedPayments">Pay now</v-btn>
             </v-card-actions>
-
-            <v-snackbar v-model="paySuccessAlert" :timeout="2000" absolute bottom left color="green">Payment Successful</v-snackbar>
         
         </v-card>
       
@@ -87,23 +57,27 @@
 <script>
     
     export default {
-        props:['classID','studentID'],
+        props:['classID','student'],
         data(){
             return{
                 
                 dialog: false,
                 valid:true,
-
-                errormsg:null,
-                
+                classFee:'',
+                amount:0+'.00',
 
                 paySuccessAlert:false,
                 
-                dayCount:null,
-                classFee:null,
-                total:null,
                 
-                getTotal:null,
+
+                table:[],
+                headers: [
+                    { text: 'DESCRIPTION',align: 'start', sortable: false, value:'description'},
+                    { text: 'FEE (Rs.)', sortable: false, value: 'classFee',width:'40%'},
+                ],
+
+                payments:[],
+                selectedPayments:[],
 
 
 
@@ -117,22 +91,34 @@
         
 
         methods:{
-            markPaid(amount){
-                if(this.$refs.form.validate()){
-                    this.paySuccessAlert=true
-                    console.log(this.studentID+" "+this.classID+" "+amount)
+            getSelect(values) {
+                this.selectedPayments = values.map(function(value){ return parseInt(value.fee) })
+                this.amount= this.selectedPayments.reduce((a,b)=>a+b,0)+'.00'
+            },
+
+            getFees(){
+                this.classFee=300
+                var paymentStatus=3
+                var payment
+                var i=0
+                if(paymentStatus>1){
+                    var arrears= paymentStatus-1
+                    for(i; i < arrears; i++){
+                        payment={no:i, description:"Arrears",fee:this.classFee}
+                        this.payments.push(payment)
+                    }
+                }
+
+                if(paymentStatus>0){
+                    payment={no:i, description:"Today fee",fee:this.classFee}
+                    this.payments.push(payment)
                 }
                 
-            }
+            },
         },
 
         created(){
-            this.dayCount=2
-            this.classFee=350
-            this.total=this.dayCount*this.classFee
-            this.getTotal=this.total
-
-
+            this.getFees()
 
         }
 
@@ -142,35 +128,18 @@
 
 
 <style scoped>
-    .theme--light.v-data-table > .v-data-table__wrapper > table > thead > tr:last-child > th {
-        border-bottom: none;
-    }
+   
 
-    .theme--light.v-data-table > .v-data-table__wrapper > table > tbody > tr:not(:last-child) > td:not(.v-data-table__mobile-row), .theme--light.v-data-table > .v-data-table__wrapper > table > tbody > tr:not(:last-child) > th:not(.v-data-table__mobile-row) {
-        border-bottom: none;
+    .centered-input >>> input {
+      text-align: center
     }
-
-    fieldset{
+  
+   fieldset{
         border-color: rgb(225, 225, 225);
         border-style: solid;
         border:0.1;
         border-style: solid;
     }
 
-    tbody tr:hover{
-        background-color: transparent !important;
-    }
-
-    
-
-    
-    @media only screen and (min-width: 768px) {
-
-
-        .v-btn:not(.v-btn--round).v-size--default{
-            width: 200px !important;
-        }
-    }
-  
 
 </style>
