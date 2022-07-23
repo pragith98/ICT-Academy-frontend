c5c3ecd94e07c328ec9a135e8b75f991030fe7fe
pragith98 <lakshanxp@hotmail.com>
2022-07-23 21:32:41 +0530

    change getAllExams api

--- src/components/ExamDetails.vue
+++ src/components/ExamDetails.vue
@@ -21,16 +21,18 @@
 
             <template>
                 <v-card flat>
-                    <v-card-title class="heading-1 blue-grey lighten-4  blue-grey--text text--darken-2">Class Details</v-card-title>
+                    <v-card-title class="heading-1 blue-grey lighten-4  blue-grey--text text--darken-2">Exam Details</v-card-title>
                     <v-card-title><v-spacer></v-spacer><v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field></v-card-title>
                     <template>
                         <div>
-                            <v-data-table :headers="headers" :items="classes" :search="search">
+                            <v-data-table :headers="headers" :items="exams" :search="search">
                                 <template v-slot:[`item.actions`]="{ item }">
                                     <v-card-actions>
-                                        <app-EditClass @success="updateSuccessAlert($event)" @failed="updateFailAlert($event)" :classDetails='item'></app-EditClass>
+                                        <v-btn :exam="item">view</v-btn>
+                                        <v-btn :exam="item">Marks</v-btn>
+                                        <!-- <app-EditClass @success="updateSuccessAlert($event)" @failed="updateFailAlert($event)" :examDetails='item'></app-EditClass> -->
                                         <v-spacer></v-spacer>
-                                        <app-DeleteClass class="ml-1" :classDetails='item' @success="deleteAlert($event)" @failed="faileAlert($event)"></app-DeleteClass>
+                                        <!-- <app-DeleteClass class="ml-1" :classDetails='item' @success="deleteAlert($event)" @failed="faileAlert($event)"></app-DeleteClass> -->
                                     </v-card-actions>
 
                                 </template>
@@ -48,13 +50,13 @@
 </template>
 
 <script>
-    import EditClass from './EditClass.vue'
-    import DeleteClass from './DeleteClass.vue'
+    // import EditClass from './EditClass.vue'
+    // import DeleteClass from './DeleteClass.vue'
 
     export default {
         components:{
-            'app-EditClass':EditClass,
-            'app-DeleteClass':DeleteClass
+            // 'app-EditClass':EditClass,
+            // 'app-DeleteClass':DeleteClass
         },
         data () {
             return {
@@ -62,22 +64,20 @@
 
                 search: '',
                 headers: [
-                    { text: 'NAME',align: 'start', sortable: false, value:'className'},
-                    { text: 'TEACHER', sortable: false, value: 'teacher.teacherName' },
-                    { text: 'GRADE',sortable: true, value: 'grade', width:'8%'},
-                    { text: 'SUBJECT', sortable: false, value: 'subject.subjectName' },
-                    { text: 'FEE', sortable: false, value: 'classFee', filterable:false},
-                    { text: 'TIME', sortable: true, value: 'time' },
-                    { text: 'LOCATION', sortable: false, value: 'room' },
-                    { text: 'DAY', sortable: true, value: 'day' },
+                    { text: 'EXAM',align: 'start', sortable: false, value:'exam'},
+                    { text: 'CLASS', sortable: false, value: 'class.className' },
+                    { text: 'GRADE',sortable: true, value: 'class.grade', width:'8%'},
+                    { text: 'BRANCH', sortable: false, value: 'branchID.branchName' },
                     { text: '', sortable: false, value: 'actions' , align:'end'},
                 ],
 
-                classes: [],
+                exams:[],
+                examsForClass: [],
+                classes:[],
 
                 breadcrumbs: [
-                    { text: 'Classes', disabled: false, href: '/Classes' },
-                    { text: 'ClassDetails', disabled: true, href: '/Classes/ClassDetails' }
+                    { text: 'Exams', disabled: false, href: '/Exams' },
+                    { text: 'ExamDetails', disabled: true, href: '/Exams/ExamDetails' }
                 ],
 
                 successAlert:false,
@@ -88,23 +88,24 @@
         },
 
         created(){
-            this.getAllClasses()
+            this.getAllExams()
         },
 
         methods: {
 
-            getAllClasses(){
-                this.axios.get(this.$apiUrl+"/api/v1.0/ClassManagement/classes",{
-                params:{
-                    status: "Active"
-                }
-                
-                }).then(Response=>(
-                    this.classes=Response.data.class.data,
+            getAllExams(){
+                this.axios.get(this.$apiUrl+"/api/v1.0/TeacherManagement/teachers/"+localStorage.getItem('userID')+"/classes").then(Response=>(
+                    this.classes=Response.data.teacher.classes,
 
                     this.classes.forEach(element => {
-                        element.time=element.startTime+"-"+element.endTime
+                        if((element.exams).length>0){
+                            element.exams.forEach(element=>{
+                                this.exams.push(element)
+                            })
+                        }
+                        
                     })
+  
                 ))
             },
 
