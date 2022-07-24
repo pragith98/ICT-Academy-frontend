4396f114e6188cd47d8a4adab63e42a58da47a04
pragith98 <lakshanxp@hotmail.com>
2022-07-24 13:38:31 +0530

    import and register examMarks componet

--- src/components/ExamMarksDetails.vue
+++ src/components/ExamMarksDetails.vue
@@ -8,24 +8,17 @@
             </v-breadcrumbs>
         <v-container>
 
-        
-            <!-- ---------------------------alerts -------------------------------------------->
-            <v-snackbar :timeout="3000" v-model="unsuccessAlert" color="red"  bottom ><v-icon left>mdi-alert-outline</v-icon> Class delete <strong>failed</strong> </v-snackbar>
-            <v-snackbar :timeout="3000" v-model="successAlert" color="green"  bottom><v-icon left>mdi-check</v-icon>Class delete <strong>successful</strong> </v-snackbar>
-            <!-- ---------------------------alerts -------------------------------------------->
 
             <template>
                 <v-card flat>
-                    <v-card-title class="heading-1 blue-grey lighten-4  blue-grey--text text--darken-2">Exam Details</v-card-title>
+                    <v-card-title class="heading-1 blue-grey lighten-4  blue-grey--text text--darken-2">Exam Marks</v-card-title>
                     <v-card-title><v-spacer></v-spacer><v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field></v-card-title>
                     <template>
                         <div>
                             <v-data-table :headers="headers" :items="exams" :search="search">
                                 <template v-slot:[`item.actions`]="{ item }">
                                     <v-card-actions>
-                                        <app-ViewExamDetails @success="reCreate($event)"  :examDetails='item'></app-ViewExamDetails>
-                                        <v-spacer></v-spacer>
-                                        <app-DeleteExam class="ml-5" @success="deleteAlert($event)" @failed="faileAlert($event)"  :examDetails='item'></app-DeleteExam>
+                                        <app-ExamMarks @success="reCreate($event)"  :examDetails='item'></app-ExamMarks>
                                     </v-card-actions>
 
                                 </template>
@@ -44,13 +37,11 @@
 
 <script>
     
-    import ViewExamDetails from './ViewExamDetails.vue'
-    import DeleteExam from './DeleteExam.vue'
+    import ExamMarks from './ExamMarks.vue'
 
     export default {
         components:{
-            'app-ViewExamDetails':ViewExamDetails,
-            'app-DeleteExam':DeleteExam,
+            'app-ExamMarks':ExamMarks,
         },
         data () {
             return {
@@ -71,11 +62,10 @@
 
                 breadcrumbs: [
                     { text: 'Exams', disabled: false, href: '/Exams' },
-                    { text: 'ExamDetails', disabled: true, href: '/Exams/ExamDetails' }
+                    { text: 'Exam Marks', disabled: true, href: '/Exams/ExamDetails' }
                 ],
 
-                successAlert:false,
-                unsuccessAlert:false,
+                
                 
             }
         },
@@ -103,20 +93,7 @@
             },
 
 
-            deleteAlert(success){
-                this.exams=[]
-                this.getAllExams();
-                this.successAlert = success;
-            },
-            faileAlert(failed){
-                this.unsuccessAlert = failed;
-            },
-
-            reCreate(success){
-                this.exams=[]
-                this.getAllExams();
-                console.log(success)
-            },
+           
         }
     }
 </script>
