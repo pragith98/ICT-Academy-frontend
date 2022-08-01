c4f77dca0dc68f7bed1fb77826e508e43ae4cb4d
pragith98 <lakshanxp@hotmail.com>
2022-08-02 01:23:24 +0530

    set user token into headers

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
+
 require('./assets/custom-globle.css');
 
 Vue.prototype.$apiUrl="http://127.0.0.1:8000"  //////////////change this address when change the backend server
@@ -10,6 +12,7 @@ Vue.prototype.$emailAddress="lakshanj1ace@gmail.com"  //////////////system email
 Vue.prototype.$emailPassword="nymefuphawvotnst"  //////////////system email password
 Vue.prototype.$tpNo="076 9198533"  //////////////system TP no.
 
+axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('userToken') ///////user token into header
 
 Vue.config.productionTip = false
 
