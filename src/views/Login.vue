3335d439958e7b2bbe9fce6d1d688801b25b3fbf
pragith98 <lakshanxp@hotmail.com>
2022-08-21 12:39:39 +0530

    remove commented code

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,75 +1,74 @@
 <template>
     <div>
         <v-img src="../assets/background.svg" aspect-ratio="1.7" style="height:100vh;">
-        <div style="position: relative;" >
-        
-            <v-img  src="../assets/login_banner.jpg" aspect-ratio="1.7" height="400">
-                <v-row class="mt-15">
-                    <v-col class="text-center">
-                        <div class="display-2 font-weight-black white--text" >WELCOME TO THE ICT ACADEMY</div>
-                        <div class="overline white--text" >Student Management and Office Automation System</div>
-                    </v-col>
-                </v-row>
-
-            </v-img>
-       
-            <div style="position: absolute;top:188px;left: 0;right: 0;">
-                <v-container>
-
-                    <v-row >
-                        <v-col cols="12" >
-                            <v-form ref="form" v-model="valid" lazy-validation>
-                                
-
-                                <v-card max-width="500"  style="margin: auto">
-                                    <v-card-title class="heading-1 blue-grey lighten-4  blue-grey--text text--darken-2">Login</v-card-title>
-                                   
-                                    <v-col cols="12" >
-                                        <v-row dense style="padding:12px">
-                                            <v-col cols="12" md="12" sm="12" >
-                                                <v-text-field v-model="userName" :rules="userNameRules"  label="Username" prepend-inner-icon="mdi-account" icon required></v-text-field>
-                                            </v-col>
-                                            <v-col cols="12" md="12" sm="12" >
-                                                <v-text-field v-model="password" :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'" :rules="passwordRules" :type="show ? 'text' : 'password'" name="input-10-2" hint="At least 8 characters" label="Password" @click:append="show = !show" prepend-inner-icon="mdi-lock" required></v-text-field>
-                                            </v-col>
-                                        </v-row>
-
-                                        <v-card-actions>
-                                            <v-row>
-                                                <v-col cols="12" md="12" sm="12">
-                                                    <v-btn :loading="loading" :disabled="!valid || !userName || !password" x-large block depressed color="primary" @click="login()">Login</v-btn>
-                                                </v-col>
-                                                <v-col cols="12" md="12" sm="12">
-                                                    <v-btn text block depressed color="primary" to="/ForgotPassword">Forgot Password?</v-btn><br>
-                                                </v-col>
-                                            </v-row>
-                                        </v-card-actions>
-                                    </v-col>
-                                    
-                                </v-card>
-                                
-                            </v-form>
-                            
+            <div style="position: relative;" >
+            
+                <v-img  src="../assets/login_banner.jpg" aspect-ratio="1.7" height="400">
+                    <v-row class="mt-15">
+                        <v-col class="text-center">
+                            <div class="display-2 font-weight-black white--text" >WELCOME TO THE ICT ACADEMY</div>
+                            <div class="overline white--text" >Student Management and Office Automation System</div>
                         </v-col>
                     </v-row>
-                    
-                </v-container>
 
-                <v-card max-width="500" style="margin: auto;top: 60px;" class="mb-4" color="error" dark flat>
-                    <v-snackbar elevation="0" max-width="500" width="500" absolute class="text-center" timeout="-1" v-model="unsuccessAlert" color="red"  ><v-icon left>mdi-alert-outline</v-icon> Username or Password is incorrect!</v-snackbar>
-                </v-card>
+                </v-img>
+        
+                <div style="position: absolute;top:188px;left: 0;right: 0;">
+                    <v-container>
+
+                        <v-row >
+                            <v-col cols="12" >
+                                <v-form ref="form" v-model="valid" lazy-validation>
+                                    
+
+                                    <v-card max-width="500"  style="margin: auto">
+                                        <v-card-title class="heading-1 blue-grey lighten-4  blue-grey--text text--darken-2">Login</v-card-title>
+                                    
+                                        <v-col cols="12" >
+                                            <v-row dense style="padding:12px">
+                                                <v-col cols="12" md="12" sm="12" >
+                                                    <v-text-field v-model="userName" :rules="userNameRules"  label="Username" prepend-inner-icon="mdi-account" icon required></v-text-field>
+                                                </v-col>
+                                                <v-col cols="12" md="12" sm="12" >
+                                                    <v-text-field v-model="password" :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'" :rules="passwordRules" :type="show ? 'text' : 'password'" name="input-10-2" hint="At least 8 characters" label="Password" @click:append="show = !show" prepend-inner-icon="mdi-lock" required></v-text-field>
+                                                </v-col>
+                                            </v-row>
+
+                                            <v-card-actions>
+                                                <v-row>
+                                                    <v-col cols="12" md="12" sm="12">
+                                                        <v-btn :loading="loading" :disabled="!valid || !userName || !password" x-large block depressed color="primary" @click="login()">Login</v-btn>
+                                                    </v-col>
+                                                    <v-col cols="12" md="12" sm="12">
+                                                        <v-btn text block depressed color="primary" to="/ForgotPassword">Forgot Password?</v-btn><br>
+                                                    </v-col>
+                                                </v-row>
+                                            </v-card-actions>
+                                        </v-col>
+                                        
+                                    </v-card>
+                                    
+                                </v-form>
+                                
+                            </v-col>
+                        </v-row>
+                        
+                    </v-container>
+
+                    <v-card max-width="500" style="margin: auto;top: 60px;" class="mb-4" color="error" dark flat>
+                        <v-snackbar elevation="0" max-width="500" width="500" absolute class="text-center" timeout="-1" v-model="unsuccessAlert" color="red"  ><v-icon left>mdi-alert-outline</v-icon> Username or Password is incorrect!</v-snackbar>
+                    </v-card>
+                </div>
+
             </div>
 
-        </div>
-
-        <!-- <template style="position: fixed;bottom:0px;"> -->
             <div style="position: fixed;bottom:10px;left: 0;right: 0;">
                 <v-footer padless color="#ECEFF1">
                     <v-col class="text-center grey--text" cols="12">© ICT Academy - Hakmana, Sri Lanka</v-col>
                 </v-footer>
             </div>
             
-        <!-- </template> -->
+
         </v-img>
     </div>
     
