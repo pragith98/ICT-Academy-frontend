3b20fb5de9a679f935ac1274bed41e688b3797d7
pragith98 <lakshanxp@hotmail.com>
2022-08-22 11:11:03 +0530

    add process cards

---
+++ src/views/Processes.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <v-breadcrumbs :items="breadcrumbs">
+            <template v-slot:divider>
+                <v-icon>mdi-chevron-right</v-icon>
+            </template>
+        </v-breadcrumbs>
+
+
+        <v-container>
+            <h1 class="title font-weight-black grey--text">PROCESSES</h1>
+            <v-row>
+                <v-col lg="3" md="4" sm="6" cols="12" v-for="cardlist in cardlist" :key="cardlist.name">
+                    <v-hover close-delay="100" v-slot="{hover}">
+                        <v-card  :elevation="hover? 7:0" flat class="'on-hover': hover lighten-2" :to=cardlist.link :color=cardlist.color dark>
+                        
+                            <v-responsive class="pt-4 text-center">
+                                <v-icon size="100">{{cardlist.icon}}</v-icon>
+                            </v-responsive>
+                            <v-card-title class=" mb-5" style="justify-content:center">
+                                <div class="title" >{{ cardlist.name }}</div>
+                            </v-card-title>
+                            
+                        </v-card>
+                    </v-hover>
+                    
+                </v-col>
+
+            </v-row>
+
+        </v-container>
+    </div>
+</template>
+
+
+
+<script>
+
+    export default{
+
+        data(){
+            return{
+                cardlist:[
+                    {name:'Month End Process', icon:'mdi-timer-cog', color:'green', link:'/Branches/CreateBranch'},
+                    {name:'Batch End Process', icon:'mdi-archive-cog', color:'orange', link:'/Branches/BranchDetails'},
+                    {name:'Year End Process', icon:'mdi-table-cog', color:'teal', link:'/Processes/YearEndProcess'}
+                ],
+
+
+                breadcrumbs: [
+                    { text: 'Processes', disabled: true, href: '/Processes' }
+                ],
+            }
+        }
+        
+    }
+</script>
