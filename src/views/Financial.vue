62d47cde7ab6b8bcec7d3ca1d7472143a767ca2e
pragith98 <lakshanxp@hotmail.com>
2022-08-22 14:32:31 +0530

    create trash card. create function to

--- src/views/Financial.vue
+++ src/views/Financial.vue
@@ -26,6 +26,22 @@
                     
                 </v-col>
 
+                <v-col lg="3" md="4" sm="6" cols="12" v-for="cardlist2 in cardlist2" :key="cardlist2.name" v-show="show">
+                    <v-hover close-delay="100" v-slot="{hover}">
+                        <v-card  :elevation="hover? 7:0" flat class="'on-hover': hover lighten-2" :to=cardlist2.link :color=cardlist2.color dark>
+                        
+                            <v-responsive class="pt-4 text-center">
+                                <v-icon size="100">{{cardlist2.icon}}</v-icon>
+                            </v-responsive>
+                            <v-card-title class=" mb-5" style="justify-content:center">
+                                <div class="title" >{{ cardlist2.name }}</div>
+                            </v-card-title>
+                            
+                        </v-card>
+                    </v-hover>
+                    
+                </v-col>
+
             </v-row>
 
         </v-container>
@@ -40,17 +56,43 @@
 
         data(){
             return{
+                show:false,
+                logedUser : JSON.parse(localStorage.getItem('user')),
+
                 cardlist:[
                     {name:'Fees Payment', icon:'mdi-cash-check', color:'pink lighten-2', link:'/Financial/FeesPayment'},
                     {name:'Teacher Advance', icon:'mdi-account-cash', color:'cyan', link:'/Financial/TeacherAdvance'},
                     {name:'Staff Advance', icon:'mdi-account-cash', color:'lime', link:'/Financial/StaffAdvance'},
-                    {name:'Expenditures', icon:'mdi-chart-donut', color:'green', link:'/Financial/Expenditure'}
+                    {name:'Expenditures', icon:'mdi-chart-donut', color:'green', link:'/Financial/Expenditure'},
+                ],
+
+                cardlist2:[
+                    {name:'Trashed', icon:'mdi-delete', color:'red', link:'/Financial/Expenditure'}
                 ],
 
 
                 breadcrumbs: [
                     { text: 'Financial', disabled: true, href: '/Financial' }
                 ],
+            }
+        },
+
+        created(){
+            this.checkPrivilege()
+        },
+
+        methods:{
+            checkPrivilege(){
+                if(this.logedUser){
+                    if(this.logedUser.privilege == 'Admin'){
+                        this.show=true
+                    }else if(this.logedUser.privilege == 'Super'){
+                        this.show=true
+                    }else{
+                        this.show=false
+                    }
+                    
+                }
             }
         }
         
