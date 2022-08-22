8291e48dbac0fa91fbc7f5bdf409e5ac4d6ea586
pragith98 <lakshanxp@hotmail.com>
2022-08-23 02:34:27 +0530

    create DeleteExpenditureTrashed component

---
+++ src/components/DeleteExpenditureTrashed.vue
@@ -0,0 +1,71 @@
+<template>
+    <v-row justify="start">
+        <v-dialog v-model="dialog" persistent max-width="400px">
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn  class="error" fab dark x-small depressed v-bind="attrs" v-on="on"><v-icon dark>mdi-delete</v-icon></v-btn>
+            </template>
+            <v-card>
+                <v-container class="text-center">
+                    <v-card-title>
+                        <v-row justify="center">
+                            <v-icon size="100" color="error">mdi-help-circle-outline</v-icon>
+                        </v-row>
+                    </v-card-title>
+                    <span class="text-h6 text-center">Do you really want to delete <br> <strong>Expenditure ID:"{{payment.expenseID}}"</strong></span>
+                </v-container>
+                
+                
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="grey" @click="dialog = false" outlined>Cancel</v-btn>
+                    <v-btn :loading="loading" color="error" @click="deletePaymentDetails()" depressed>Delete
+                        <v-icon right>mdi-delete</v-icon>
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-row>
+</template>
+
+
+
+<script>
+    export default {
+        props:['payment'],
+        data: () => ({
+            dialog: false,
+            loading:false,
+        }),
+
+        methods:{
+
+            deletePaymentDetails(){
+                this.loading=true
+                this.axios.delete(this.$apiUrl+'/api/v1.0/ExpenditureManagement/expenditures/'+this.payment.expenseID+'/trashed')
+                    .then(Response=>{
+                        if(Response.data.success == true){
+                            this.dialog = false
+                            this.successAlert();
+                            this.loading=false
+                        }else{
+                            this.failedAlert();
+                        }
+                    })
+                    .catch(error => {
+                        this.loading=false
+                        this.failedAlert()
+                        console.log(error.data)
+                        
+                    });
+            },
+
+
+            successAlert(){
+                this.$emit('success',true)
+            },
+            failedAlert(){
+                this.$emit('failed',true)
+            }
+        }
+    }
+</script>
