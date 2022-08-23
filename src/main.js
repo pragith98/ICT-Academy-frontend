59d79a467119fe38c4a051d854a66e45dfca10e1
pragith98 <lakshanxp@hotmail.com>
2022-08-24 01:12:00 +0530

    install jspdf

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 
+
 require('./assets/custom-globle.css');
+
+
 
 Vue.prototype.$apiUrl="http://127.0.0.1:8000"  //////////////change this address when change the backend server
 
@@ -28,6 +31,7 @@ if(logedUser){
 Vue.config.productionTip = false
 
 new Vue({
+
   router,
   vuetify,
   render: h => h(App)
